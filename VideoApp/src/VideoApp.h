edb5aaf7ad46f16b346db09aa9d50c354593d204
Radoslav Yordanov <r.yordanov@dopamine.bg>
2020-10-30 10:20:18 +0200

    draw little red square with opengl

--- VideoApp/src/VideoApp.h
+++ VideoApp/src/VideoApp.h
@@ -6,6 +6,15 @@
 #ifndef VIDEOAPP_H
 #define VIDEOAPP_H
 
+#ifndef WINDOW_WIDTH
+#define WINDOW_WIDTH 640
+#endif // !WINDOW_WIDTH
+
+#ifndef WINDOW_HEIGHT
+#define WINDOW_HEIGHT 480
+#endif // !1
+
+
 void error_callback(int error, const char* description);
 void windows_close_callback(GLFWwindow* window);
 
--- VideoApp/src/main.cpp
+++ VideoApp/src/main.cpp
@@ -13,9 +13,9 @@ int main(int argc, const char** argv)
 	}
 
 	
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello World", NULL, NULL);
 
-	glfwSetWindowSizeLimits(window, 640, 480, GLFW_DONT_CARE, GLFW_DONT_CARE);
+	glfwSetWindowSizeLimits(window, WINDOW_WIDTH, WINDOW_HEIGHT, GLFW_DONT_CARE, GLFW_DONT_CARE);
 
 	if (!window) {
 		printf("Couldn`t open window\n");
@@ -25,10 +25,24 @@ int main(int argc, const char** argv)
 		return 1;
 	}
 
+	unsigned char* data = new unsigned char[100 * 100 * 3];
+
+	for (int y = 0; y < 100; y++) {
+		for (int x = 0; x < 100; x++) {
+			data[y * 100 * 3 + x * 3	] = 0xff;
+			data[y * 100 * 3 + x * 3 + 1] = 0x00;
+			data[y * 100 * 3 + x * 3 + 2] = 0x00;
+		}
+	}
+
 	glfwMakeContextCurrent(window);
 	glfwSetWindowCloseCallback(window, windows_close_callback);
 
 	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glDrawPixels(100, 100, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glfwSwapBuffers(window);
+
 		glfwWaitEvents();
 	}
 
